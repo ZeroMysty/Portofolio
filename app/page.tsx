b26c0a28c3e81f599af3313c67b1e83888b76616
Zaid Hifdzul Mumtaz <zaidmumtaz68@gmail.com>
2026-04-10 09:42:46 +0700

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <h1>Home</h1>
       <p>Welcome to my website!</p>
       <p>I'm a software engineer with a passion for building cool stuff.</p>
+      <p>Makan bg</p>
       
     </main>
   )
